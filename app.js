4f4a60df30d06aef5edb5696d10388d800790e6e
teccis <ventasteccis@gmail.com>
2022-02-24 20:39:13 -0600

    add js file

---
+++ app.js
@@ -0,0 +1,11 @@
+(function(){
+    'use strict';
+
+    angular.mode('NameCalculator',[])
+
+    .controller('NameCalculatorController', function($scope){
+        $scope.name="";
+        $scope.totalValue=0;
+    });
+
+})();
